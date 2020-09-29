38381ba00ed22d3a5bc9be34fd84819d8a36ae34
keavy-isabel <72107417+keavy-isabel@users.noreply.github.com>
2020-09-29 17:30:41 -0400

    Add files via upload

--- Exercise01-Result/exercise01-result.py
+++ Exercise01-Result/exercise01-result.py
@@ -1,23 +1,27 @@
-#creando la lista vacía
+#creando lista vacía
 listaRegistro = []
 suma = 0
 clientes = 0
 
+#Indicacioens
 print("Ingrese la cantidad de clientes que desee. Una vez ya no quiera seguir introduciendo registros, digite un espacio cuando le pida el nombre del cliente. Inmediatamente le aparecerá un mensaje con el costo total ingresado")
+
+#Inicio del bucle
 while True:
     cliente = input("Nombre del cliente: ")
     if cliente != " ":
         producto = input("Producto: ")
         costo = float(input("Costo ($0.00):"))
-        # punto de programa
+        # Crea directorio
         registro = dict(cliente=cliente, producto=producto, costo=costo)
-        #como agrego un elemento nuevo a una lista?
+        #Agrega nuevo elementos a la lista
         listaRegistro.append(registro)
-        #dejar de ocupar la variable registro
-        # #registro = None
+        #suma cada costo que se agrega
         suma += (listaRegistro[clientes]["costo"])
+        #aumenta el número de cliente registrado
         clientes+=1
     else:
+        #termina el bucle
         break
-
+#Imprime total del costo
 print("El costo obtenido hasta ahora es de: " , suma)
